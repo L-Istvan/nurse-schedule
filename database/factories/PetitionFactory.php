00726554a99fa29c6f79971193b27472c1546d64
L.Istvan <laczipistii@gmail.com>
2023-03-06 18:28:05 +0100

    create

---
+++ database/factories/PetitionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Petition>
+ */
+class PetitionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $faker = \Faker\Factory::create();
+        $randtime = $faker->dateTimeBetween("2023-03-01", "2023-03-25");
+        return [
+            'user_id' =>4,
+            'date' => $randtime,
+        ];
+    }
+}
