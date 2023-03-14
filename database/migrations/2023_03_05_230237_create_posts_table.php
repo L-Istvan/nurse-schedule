f960d20223eff5e96d0c58cebd7eea8ce5571574
L.Istvan <laczipistii@gmail.com>
2023-03-14 02:47:14 +0100

    -

--- database/migrations/2023_03_05_230237_create_posts_table.php
+++ database/migrations/2023_03_05_230237_create_posts_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->integer('person_id');
             $table->date('date');
             $table->integer('position');
             $table->timestamps();
