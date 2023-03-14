3d4efc922647700c11a3ff951eb69afc2024d1f3
L.Istvan <laczipistii@gmail.com>
2023-03-14 02:58:51 +0100

    add calendar counter

--- resources/views/nursePages/index.blade.php
+++ resources/views/nursePages/index.blade.php
@@ -2,12 +2,14 @@
 @section('content')
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js'></script>
 <script src="{{ asset('js/nursePageIndex.js') }}"></script>
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <style>
     #calendar {
       max-height: 800px;
       overflow-y: auto;
     }
   </style>
+
 
 <div class="content-wrapper">
   <!-- Main content -->
@@ -23,17 +25,37 @@
               <div class="card-body">
                 <!-- the events -->
                 <div class="card">
-                <h5 class="text-center mt-3 mb-3">Kérés: 10/2</h5>
-                <h5 class="text-center mb-3">Havi Szabadság: 10/3</h5>
-                <h5 class="text-center mb-3">Éves szabadság: 10/5</h5>
-                <h5 class="text-center mb-4">Beteg szabadság: 10</h5>
+                @if ($settingArray->first())
+                    <h5 class="text-center mt-3 mb-3">Kérés:
+                        <span id="maxPetitons">{{$settingArray->first()->maxPetitons}} </span> /
+                        <span id="currentPetitons">{{$settingArray->first()->currentPetitons}}</span>
+                    </h5>
+                    <h5 class="text-center mb-3">Havi Szabadság:
+                        <span id="maxMonthHoliday">{{$settingArray->first()->maxMonthHoliday}} </span> /
+                        <span id="currentMonthHoliday">{{$settingArray->first()->currentMonthHoliday}}</span>
+                    </h5>
+                    <h5 class="text-center mb-3">Éves szabadság:
+                        <span id="maxYearHoliday">{{$settingArray->first()->maxYearHoliday}} </span> /
+                        <span id="currentYearHoliday">{{$settingArray->first()->currentYearHoliday}}</span>
+                    </h5>
+                    <h5 class="text-center mb-4">Beteg szabadság:
+                        0
+                    </h5>
+                @else
+                    <h5 class="text-center mt-3 mb-3">Kérés: - / -</h5>
+                    <h5 class="text-center mb-3">Havi Szabadság: - / -
+                    </h5>
+                    <h5 class="text-center mb-3">Éves szabadság: - / -
+                    </h5>
+                    <h5 class="text-center mb-4">Beteg szabadság: -
+                    </h5>
+                @endif
                 </div>
                 <div id="external-events">
                   <button class="btn btn-secondary btn-lg btn-block" onclick="keres()">Kérés</button>
                   <button class="btn btn-primary btn-lg btn-block" onclick="szabadsag()">Szabadság</button>
                   <button class="btn btn-danger btn-lg btn-block" onclick="betegSzabadsag()">Beteg szabadság</button>
-                  <button class="btn btn-danger btn-lg btn-block" onclick="mentes()">Mentés</button>
-                  <button class="btn btn-danger btn-lg btn-block"  id="getevent">Click me</button>
+                  <button class="btn btn-danger btn-lg btn-block" onclick="save()">Mentés</button>
                 </div>
               </div>
             </div>
