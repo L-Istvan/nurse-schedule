31d2ce13cf2a2a6cfcbcf01bc6208515b460fa02
L.Istvan <laczipistii@gmail.com>
2023-03-23 04:28:47 +0100

    table completed

---
+++ resources/views/headNursePages/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+@section('content')
+
+<style>
+/****** Tábla ****/
+tr{
+    cursor: pointer; transition: all .25s ease-in-out
+}
+
+table, th, td {
+    border:1px solid black;
+    border-collapse: collapse;
+    table-layout: fixed;
+    width : auto;
+    min-width: 30px;
+    min-height: 30px;
+    margin-top: 30px;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+  }
+
+</style>
+
+
+<div class="content-wrapper">
+    <div style="overflow-x:auto;">
+    <table id=table>
+        {{-- Napok ki iratása--}}
+        @for ($i=0;$i < Carbon\Carbon::now()->daysInMonth+1;$i++)
+            @if ($i == 0 ) <th scope=col>Nap</th>
+            @else <th scope='col'>{{$i}}</th>
+            @endif
+        @endfor
+        {{--  --}}
+        @for ($i=0;$i<5;$i++)
+            <tr id={{$i}}> {{-- új sor és id--}}
+            <th scope="row">alma</th>
+            @for ($j=0;$j<Carbon\Carbon::now()->daysInMonth;$j++)
+                <td> </td>
+            @endfor
+            </tr>
+        @endfor
+    </table>
+    </div>
+
+    <script src="{{ asset('js/HeadNursePages/edit.js') }}"></script>
+</div>
+
+
+
+@endsection
