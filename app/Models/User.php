436c63b3a4c6c3711623ae86d0baa580c90207e1
L.Istvan <laczipistii@gmail.com>
2023-03-23 04:18:48 +0100

    -

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,9 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'group_id',
         'name',
+        'mobile_number',
         'email',
         'password',
         'role',
