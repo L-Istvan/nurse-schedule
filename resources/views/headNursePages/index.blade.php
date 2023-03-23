89403436d94defc7c1aba04d929f28c6bd777d58
L.Istvan <laczipistii@gmail.com>
2023-03-23 04:29:34 +0100

    "final" design completed

--- resources/views/headNursePages/index.blade.php
+++ resources/views/headNursePages/index.blade.php
@@ -1,12 +1,90 @@
 @extends('layouts.app')
 @section('content')
+<script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js'></script>
+    <script>
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth',
+          dayBorderColor: 'black',
+        });
+        calendar.render();
+      });
+    </script>
 
-<div class="content-wrapper">
-    <p>Főnővér</p>
-    <form method="POST" action="{{ route('logout') }}">
-        @csrf
+<style>
+    .vmi{
+        background-image: url('/images/opacity_background.JPEG');
+        background-size: cover;
+    }
+    #calendar{
+        background-color: rgba(255,255,255, 0.2);
+        color:black;
+    }
+    .card{
+        background-color: rgba(0,0,0, 0.2);
+    }
+    .fc-theme-standard td, .fc-theme-standard th {
+        border: 1px solid #000;
+        border-right-width: 1.5px;
+        border-bottom-width: 1.5px;
+    }
+    .fc-theme-standard .fc-scrollgrid {
+        border: 1px solid #000;
+    }
+    .fc .fc-daygrid-day.fc-day-today {
+        background-color: rgba(255,255,255, 0.4);
+    }
+    .btn-success{
+        background-color: rgb(9, 147, 20);
+    }
+</style>
 
-        <button type="submit">Kijelentkezés</button>
-    </form>
-</div>
-@endsection
+<div class="content-wrapper vmi">
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="sticky-top mt-5">
+              <div class="card">
+                <div class="card-header text-white">
+                  <h5 class="text-center">Felhasznált</h5>
+                </div>
+                <div class="card-body">
+                  <!-- the events -->
+                  <div class="card text-white">
+                      <h5 class="text-center mt-3 mb-3">Kérés: - / -</h5>
+                      <h5 class="text-center mb-3">Havi Szabadság: - / -
+                      </h5>
+                      <h5 class="text-center mb-3">Éves szabadság: - / -
+                      </h5>
+                      <h5 class="text-center mb-4">Beteg szabadság: -
+                      </h5>
+                  </div>
+                  <div id="external-events">
+                    <button class="btn btn-success btn-lg btn-block" onclick="keres()">Kérés</button>
+                    <button class="btn btn-primary btn-lg btn-block" onclick="szabadsag()">Szabadság</button>
+                    <button class="btn btn-danger btn-lg btn-block" onclick="betegSzabadsag()">Beteg szabadság</button>
+                    <button class="btn btn-dark btn-lg btn-block" onclick="save()">Mentés</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-md-9">
+
+                <!-- THE CALENDAR -->
+                <div id="calendar"></div>
+            <!-- /.card -->
+          </div>
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  <!-- /.content-wrapper -->
+  </div>
+  @endsection
--- resources/views/nursePages/index.blade.php
+++ resources/views/nursePages/index.blade.php
@@ -1,30 +1,55 @@
 @extends('layouts.app')
 @section('content')
 <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.4/index.global.min.js'></script>
-<script src="{{ asset('js/nursePageIndex.js') }}"></script>
+<script src="{{ asset('js/NursePages/index.js') }}"></script>
+<link href="toastr.css" rel="stylesheet"/>
+
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <style>
     #calendar {
-      max-height: 800px;
-      overflow-y: auto;
+      min-height: 490px; /* vagy más, Önnek megfelelő érték */
+      max-height: 1200px;
+      background-color: rgba(255,255,255, 0.2);
+      color:black;
+    }
+    .vmi{
+        background: url('/images/opacity_background.JPEG');
+      background-size: cover;
+    }
+    .card{
+        background-color: rgba(0,0,0, 0.2);
+    }
+    .fc-theme-standard td, .fc-theme-standard th {
+        border: 1px solid #000;
+        border-right-width: 1.5px;
+        border-bottom-width: 1.5px;
+    }
+    .fc-theme-standard .fc-scrollgrid {
+        border: 1px solid #000;
+    }
+    .fc .fc-daygrid-day.fc-day-today {
+        background-color: rgba(255,255,255, 0.4);
+    }
+    .btn-success{
+        background-color: rgb(9, 147, 20);
     }
   </style>
 
 
-<div class="content-wrapper">
+<div class="content-wrapper vmi">
   <!-- Main content -->
   <section class="content">
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-3">
-          <div class="sticky-top mb-3">
+          <div class="sticky-top mt-5">
             <div class="card">
-              <div class="card-header">
+              <div class="card-header text-white">
                 <h5 class="text-center">Felhasznált</h5>
               </div>
               <div class="card-body">
                 <!-- the events -->
-                <div class="card">
+                <div class="card text-white">
                 @if ($settingArray->first())
                     <h5 class="text-center mt-3 mb-3">Kérés:
                         <span id="maxPetitons">{{$settingArray->first()->maxPetitons}} </span> /
@@ -39,7 +64,7 @@
                         <span id="currentYearHoliday">{{$settingArray->first()->currentYearHoliday}}</span>
                     </h5>
                     <h5 class="text-center mb-4">Beteg szabadság:
-                        0
+                        <span id="SickLeaves">{{$settingArray->first()->SickLeaves}} </span>
                     </h5>
                 @else
                     <h5 class="text-center mt-3 mb-3">Kérés: - / -</h5>
@@ -52,10 +77,10 @@
                 @endif
                 </div>
                 <div id="external-events">
-                  <button class="btn btn-secondary btn-lg btn-block" onclick="keres()">Kérés</button>
+                  <button class="btn btn-success btn-lg btn-block" onclick="keres()">Kérés</button>
                   <button class="btn btn-primary btn-lg btn-block" onclick="szabadsag()">Szabadság</button>
                   <button class="btn btn-danger btn-lg btn-block" onclick="betegSzabadsag()">Beteg szabadság</button>
-                  <button class="btn btn-danger btn-lg btn-block" onclick="save()">Mentés</button>
+                  <button class="btn btn-dark btn-lg btn-block" onclick="save()">Mentés</button>
                 </div>
               </div>
             </div>
@@ -63,13 +88,8 @@
         </div>
         <!-- /.col -->
         <div class="col-md-9">
-          <div class="card card-primary">
-            <div class="card-body p-0">
               <!-- THE CALENDAR -->
               <div id="calendar"></div>
-            </div>
-            <!-- /.card-body -->
-          </div>
           <!-- /.card -->
         </div>
         <!-- /.col -->
@@ -79,6 +99,5 @@
   </section>
   <!-- /.content -->
 <!-- /.content-wrapper -->
-
 </div>
 @endsection
