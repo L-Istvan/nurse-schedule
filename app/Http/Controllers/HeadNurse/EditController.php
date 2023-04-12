12c8373d2befa4a9e3747e1ed71203ca38a7680a
L.Istvan <laczipistii@gmail.com>
2023-04-12 21:29:08 +0200

    starter

--- app/Http/Controllers/HeadNurse/EditController.php
+++ app/Http/Controllers/HeadNurse/EditController.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\HeadNurse;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Post;
 
-class headNurseEditController extends Controller
+class EditController extends Controller
 {
-
+    public function index(){
+        $getUserName = User::getUserName(Auth::user()->id);
+        $shift = Post::getAllData(Auth::user()->id);
+        return view('headNursePages/edit',['userName' => $getUserName]);
+    }
 }
