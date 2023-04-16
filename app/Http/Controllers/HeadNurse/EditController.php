aaf01cf4c5db53227a213aa594ac999ae6f223eb
L.Istvan <laczipistii@gmail.com>
2023-04-16 23:15:44 +0200

    save in post table (not ready yet)

--- app/Http/Controllers/HeadNurse/EditController.php
+++ app/Http/Controllers/HeadNurse/EditController.php
@@ -2,17 +2,48 @@
 
 namespace App\Http\Controllers\HeadNurse;
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use App\Models\Post;
+
+use Illuminate\Auth\Events\Validated;
+use Barryvdh\Debugbar\Facades\Debugbar;
+
+use function Symfony\Component\String\b;
+use Illuminate\Support\Facades\Validator;
 
 class EditController extends Controller
 {
     public function index(){
-        $getUserName = User::getUserName(Auth::user()->id);
+        $getUser = User::getEmailandNamebyUser(Auth::user()->id);
         $shift = Post::getAllData(Auth::user()->id);
-        return view('headNursePages/edit',['userName' => $getUserName]);
+        return view('headNursePages/edit',["getUser" => $getUser]);
+    }
+
+    public function store(Request $request){
+        $data = $request->all();
+        $validator = Validator::make($data, [
+            '*.email' => ['required', 'string', 'email', 'max:255'],
+            '*.date' => ['required','string','date'],
+            '*.shift' => ['required','numeric','in:1,2'],
+        ]);
+
+        if ($validator->passes()) {
+
+            try {
+                foreach($data as $item) {
+                    $post = new Post(Auth::user()->id,100,$item['date'],$item['shift']);
+                }($data);
+            } catch (\Throwable $th) {
+                //throw $th;
+            }
+            return response('Sikeres mentés',200);
+        } else {
+            $errors = $validator->errors()->all();
+            Debugbar::error($errors);
+        }
+        return response($errors,500);
     }
 }
