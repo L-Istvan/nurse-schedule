53f3005a8a20361f91312be4c491120d5a08646a
L.Istvan <laczipistii@gmail.com>
2023-04-16 23:19:34 +0200

    table header preapared

--- resources/views/headNursePages/edit.blade.php
+++ resources/views/headNursePages/edit.blade.php
@@ -1,31 +1,42 @@
 @extends('layouts.app')
 @section('content')
-
-<style>
-/****** Tábla ****/
-tr{
-    cursor: pointer; transition: all .25s ease-in-out
-}
-
-table, th, td {
-    border:1px solid black;
-    border-collapse: collapse;
-    table-layout: fixed;
-    width : auto;
-    min-width: 30px;
-    min-height: 30px;
-    margin-top: 30px;
-    margin-left: auto;
-    margin-right: auto;
-    text-align: center;
-  }
-
-</style>
-
+<meta name="csrf-token" content="{{ csrf_token() }}">
+<link rel="stylesheet" href="{{ asset('css/edit.css') }}">
 
 <div class="content-wrapper">
-    <div style="overflow-x:auto;">
-    <table id=table>
+    <div class="box">
+            <div class="month">
+                <ul>
+                  <li class="prev" onclick="prev()">&#10094;</li>
+                  <li class="next" onclick="next()">&#10095;</li>
+                  <li>
+                    <label id="month"></label><br>
+                    <span id="year" style="font-size:18px"></span>
+                  </li>
+                </ul>
+            </div>
+    </div>
+        <div class="row d-flex justify-content-center mt-1">
+          <div class="col-auto">
+            <div class="d-flex align-items-center">
+                <div class="square ml-1" style="background-color: #3e7eb3"></div>
+                <span class="align-middle mr-3">Nappal</span>
+                <div class="square" style="background-color: #393e46;"></div>
+                <span>Éjszaka</span>
+            </div>
+          </div>
+          <div class="col-auto flex-grow-1 text-center">
+            <button class="">Beosztás készítés</button>
+            <button class="" onclick="save()">Beosztás mentés</button>
+          </div>
+          <div class="col-auto invisible">
+            Nappal   Éjszaka
+          </div>
+        </div>
+
+
+
+    <table id="table" class="mt-3">
         {{-- Napok ki iratása--}}
         @for ($i=0;$i < Carbon\Carbon::now()->daysInMonth+1;$i++)
             @if ($i == 0 ) <th scope=col>Nap</th>
@@ -33,19 +44,16 @@ table, th, td {
             @endif
         @endfor
         {{--  --}}
-        @foreach ($userName as $name)
-            <tr id={{$i}}> {{-- új sor és id--}}
-            <th scope="row">{{ $name }}</th>
+        @foreach ($getUser as $user)
+        <tr id= {{ $user['email'] }}>  {{-- új sor és id--}}
+            <th scope="row">{{ $user['name'] }}</th>
             @for ($j=0;$j<Carbon\Carbon::now()->daysInMonth;$j++)
                 <td> </td>
             @endfor
-            </tr>
+        </tr>
         @endforeach
     </table>
-    </div>
     <script src="{{ asset('js/HeadNursePages/edit.js') }}"></script>
 </div>
-
-
 
 @endsection
