6254abde6f6d32abbd7ef89d0e2164638e48ed8f
L.Istvan <laczipistii@gmail.com>
2023-04-16 23:18:45 +0200

    table header prepared

--- public/js/HeadNursePages/edit.js
+++ public/js/HeadNursePages/edit.js
@@ -1,9 +1,47 @@
 const array = [];
 
+//Month
+const d = new Date();
+var monthNumber = d.getMonth();
+var year = d.getFullYear();
+const months = ["Január","Február","Március","Április","Május","Június",
+    "Július","Agusztus","Szeptember","Oktober","November","December"];
+document.getElementById("month").innerHTML = months[monthNumber];
+function prev(){
+    if (monthNumber == 0){
+        monthNumber = 11;
+        year--;
+        document.getElementById("year").innerHTML = year;
+        document.getElementById("month").innerHTML = months[monthNumber];
+    }
+    else{
+        monthNumber--;
+        document.getElementById("month").innerHTML = months[monthNumber];
+    }
+
+}
+function next(){
+    if(monthNumber == 11){
+        monthNumber = 0;
+        year++;
+        document.getElementById("year").innerHTML = year;
+        document.getElementById("month").innerHTML = months[monthNumber];
+    }
+    else{
+        monthNumber++;
+        document.getElementById("month").innerHTML = months[monthNumber];
+    }
+}
+
+//Year
+document.getElementById("year").innerHTML = year;
+
+
+
 /*----------Colors-------------*/
 $colorBackground = "";
-$day = "rgb(0, 173, 181)"; // sárga --> value = 0
-$night = "rgb(57, 62, 70)"; //nagyaransá --> value = 1
+$day = "rgb(62, 126, 179)"; // blue --> value = 2
+$night = "rgb(57, 62, 70)"; //gray --> value = 1
 
 /*------Fegyeli a táblázatot szín módosítás céljából-------*/
 const tbody = document.querySelector('table');
@@ -16,14 +54,14 @@ tbody.addEventListener('click', function (e) {
     x = document.getElementById("table").rows[row.rowIndex].cells[cell.cellIndex];
     if (window.getComputedStyle(e.target,null).backgroundColor == "rgba(0, 0, 0, 0)" ||
     window.getComputedStyle(e.target,null).backgroundColor == "rgb(0, 0, 255)"){ //eredeti szín -e
-        x.style.backgroundColor = "#00ADB5"; //vilagos kek
+        x.style.backgroundColor = "#3e7eb3"; //vilagos kek
         }
     else{
-        if (window.getComputedStyle(e.target,null).backgroundColor == "rgb(0, 173, 181)"){ //sárga -e
-            x.style.backgroundColor = "#393e46"; //sotet kek
+        if (window.getComputedStyle(e.target,null).backgroundColor == "rgb(62, 126, 179)"){ //kék -e
+            x.style.backgroundColor = "#393e46"; //szürke
         }
         else{
-            if (window.getComputedStyle(e.target,null).backgroundColor == "rgb(57, 62, 70)"){ //narancs -e
+            if (window.getComputedStyle(e.target,null).backgroundColor == "rgb(57, 62, 70)"){ //szürke -e
                 x.style.backgroundColor = "rgba(0, 0, 0, 0)";
             }
             else{
@@ -56,4 +94,43 @@ function tableload() {
             }
         }
     }
+}
+
+
+function save() {
+    $count = 0;
+    $value = 0;
+    var table = document.getElementById('table');
+    for (var r = 0, n = table.rows.length; r < n; r++) {
+        for (var c = 0, m = table.rows[r].cells.length; c < m; c++) {
+            // Day
+            if (window.getComputedStyle(table.rows[r].cells[c],null).backgroundColor == $day){
+                //                  dolgozoID            nap         muszak
+                array[$count++] = {"email": table.rows[r].id,"date": year+"-"+monthNumber+"-"+c,"shift": 2}; //
+            }
+            // night
+            if (window.getComputedStyle(table.rows[r].cells[c],null).backgroundColor ==  $night){
+                //                  dolgozoID            nap         muszak
+                array[$count++] = {"email": table.rows[r].id,"date": year+"-"+monthNumber+"-"+c,"shift": 1}; //
+            }
+        }
+    }
+
+    $.ajax({
+        headers:{
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        },
+        type: 'POST',
+        url: '/saveTableCells',
+        data : JSON.stringify(array),
+        contentType: 'application/json',
+        complete: function(xhr){
+            if (xhr.status == 200) toastr.success(xhr.responseText);
+            else toastr.error(xhr.responseText);
+        }
+    });
+}
+
+function valami(){
+    console.log("hello");
 }
