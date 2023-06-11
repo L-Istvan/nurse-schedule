3c1bc6bf84851e46f9bfd4df617302721e32b5aa
L.Istvan <laczipistii@gmail.com>
2023-06-11 17:08:45 +0200

    without header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,16 +16,16 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <!-- Toasts -->
         <link rel="stylesheet" href="{{ asset('dist/css/toastr.min.css') }}">
+        <!-- -->
     </head>
     <body class="hold-transition sidebar-mini">
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
     <!--- <body class="hold-transition dark-mode sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed"> -->
         <div class="wrapper">
-        @include('layouts.header')
+        {{--@include('layouts.header')--}}
         @include('layouts.sidebar')
         @yield('content')
-        <!-- AdminLTE App -->
         <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
         <script src="{{ asset('dist/js/toastr.min.js') }}"></script>
         <script src="{{ asset('/dist/js/bootstrap.bundle.min.js') }}"></script>
