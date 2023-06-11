025913d6d3aa9bafd159ddfca11b43e25c986992
L.Istvan <laczipistii@gmail.com>
2023-06-11 17:25:04 +0200

    add some query

--- app/Models/SickLeave.php
+++ app/Models/SickLeave.php
@@ -32,4 +32,10 @@ class SickLeave extends Model
     public static function searchDate($person_id,$date){
         return self::where('person_id', $person_id)->where('date', $date)->exists();
     }
+
+    public static function getSickLeavebyMonthAndUserId($person_id,$month){
+        return self::where('person_id',$person_id)
+        ->whereMonth('date', $month)
+        ->pluck('date');
+    }
 }
