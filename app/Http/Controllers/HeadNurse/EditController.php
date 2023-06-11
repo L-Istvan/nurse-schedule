13c857575550ae7443bf0361cf73b9b24b87c1ba
L.Istvan <laczipistii@gmail.com>
2023-06-11 17:27:33 +0200

    Fix the database save and add destroy(delete)

--- app/Http/Controllers/HeadNurse/EditController.php
+++ app/Http/Controllers/HeadNurse/EditController.php
@@ -2,48 +2,64 @@
 
 namespace App\Http\Controllers\HeadNurse;
 
+use Exception;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
 use Illuminate\Support\Facades\Auth;
-
-use Illuminate\Auth\Events\Validated;
 use Barryvdh\Debugbar\Facades\Debugbar;
-
-use function Symfony\Component\String\b;
 use Illuminate\Support\Facades\Validator;
 
 class EditController extends Controller
 {
     public function index(){
-        $getUser = User::getEmailandNamebyUser(Auth::user()->id);
-        $shift = Post::getAllData(Auth::user()->id);
+        $getUser = User::getUserIdAndNamebyUser(Auth::user()->id);
         return view('headNursePages/edit',["getUser" => $getUser]);
     }
 
     public function store(Request $request){
         $data = $request->all();
         $validator = Validator::make($data, [
-            '*.email' => ['required', 'string', 'email', 'max:255'],
+            '*.id' => ['required', 'numeric'],
             '*.date' => ['required','string','date'],
             '*.shift' => ['required','numeric','in:1,2'],
         ]);
 
         if ($validator->passes()) {
-
             try {
                 foreach($data as $item) {
-                    $post = new Post(Auth::user()->id,100,$item['date'],$item['shift']);
-                }($data);
-            } catch (\Throwable $th) {
-                //throw $th;
+                    $post = Post::firstOrCreate([
+                        'group_id' => Auth::user()->id,
+                        'person_id' => $item['id'],
+                        'date' => $item['date'],
+                        'position' => $item['shift']
+                    ]);
+                }
+            } catch (Exception $ex) {
+                Debugbar::info($ex);
             }
             return response('Sikeres mentés',200);
         } else {
             $errors = $validator->errors()->all();
-            Debugbar::error($errors);
         }
         return response($errors,500);
+    }
+
+    public function destroy(Request $request){
+        $data = $request->all();
+        try {
+            Post::deleteRow(Auth::user()->id,$data[0],$data[1]);
+            return response('Sikeres törlés',200);
+        } catch (Exception $ex) {
+            return response('Sikertelen törlés');
+        }
+    }
+
+    public function tableLoad(Request $request){
+        $data = $request->all();
+        $shift = Post::getShift(Auth::user()->id);
+        return response()->json([$shift],200);
     }
 }
