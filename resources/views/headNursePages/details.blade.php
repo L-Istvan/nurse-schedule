ed508be3f064b8110153fbf378ce0a852538450e
L.Istvan <laczipistii@gmail.com>
2023-06-20 13:50:38 +0200

    -

--- resources/views/headNursePages/details.blade.php
+++ resources/views/headNursePages/details.blade.php
@@ -5,44 +5,15 @@
 
 
 <div class="content-wrapper" style="background-color: rgb(237, 242, 248);">
-    <div class="box">
-        <div class="month">
-            <ul>
-                <a class="nav-link" data-widget="pushmenu" role="button"><i class="fas fa-bars"></i></a>
-              <li>
-                    <div class="container-fluid">
-                      <div class="d-flex justify-content-center">
-                          <a href="#" class="btn btn-m" data-toggle="modal" data-target="#exampleModal" style="border-radius: 1rem;background-color:#3e7eb3;color:white;box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);">
-                              <img id="regist" src="/dist/img/addemployee.png" alt="">Új nővér regisztálás
-                          </a>
-                      </div>
-                    </div>
-              </li>
-            </ul>
+    <div class="row">
+        <div class="col-6">
+            alma
         </div>
+        <div class="col-6">
+            korte
+        </div>
+
     </div>
-
-
-    @if (session('err'))
-    <div class="alert alert-danger">
-        {{ session('succes') }}
-    </div>
-@elseif ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
-
-
-
-
-
-
 
 
  </div>
